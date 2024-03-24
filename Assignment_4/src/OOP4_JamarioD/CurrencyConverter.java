733f6d5b15087cb2ae2569a638100bde1f8bd5cb
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-03-24 01:12:17 -0500

    Assignment 4 Events added Try catch statments added
    Program function so far. But final review required.

--- Assignment_4/src/OOP4_JamarioD/CurrencyConverter.java
+++ Assignment_4/src/OOP4_JamarioD/CurrencyConverter.java
@@ -1,6 +1,11 @@
 package OOP4_JamarioD;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.util.function.ToDoubleFunction;
 
 public class CurrencyConverter {
     private JPanel mainForm;
@@ -9,9 +14,81 @@ public class CurrencyConverter {
     private JTextField tfJMDResults;
     private JButton bConvert;
     private JButton bClear;
+    private JLabel inputLabel;
+    private double uSToJMD = 129.02;
+    private double cadToJMD = 97.50;
+    private double euroToJMD = 164.33;
 
     public CurrencyConverter() {
 
+        bConvert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) throws NumberFormatException {
+
+                try {
+                    double result;
+                    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+                    String formattedAmount;
+
+                    if(!tfInput.getText().isEmpty())
+                    {
+                        switch(cbCurrencyType.getSelectedIndex())
+                        {
+                            case 0:
+                                result = (Double.parseDouble(tfInput.getText())) * uSToJMD;
+                                formattedAmount = currencyFormatter.format(result);
+                                tfJMDResults.setText(formattedAmount);
+                                break;
+                            case 1:
+                                result = (Double.parseDouble(tfInput.getText())) * cadToJMD;
+                                formattedAmount = currencyFormatter.format(result);
+                                tfJMDResults.setText(formattedAmount);
+                                break;
+                            case 2:
+                                result = (Double.parseDouble(tfInput.getText())) * euroToJMD;
+                                formattedAmount = currencyFormatter.format(result);
+                                tfJMDResults.setText(formattedAmount);
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Invalid Selection", "Error", JOptionPane.ERROR_MESSAGE);
+                                break;
+
+                        }
+                    } else
+                    {
+                        JOptionPane.showMessageDialog(null, "Please enter a value to convert!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (NumberFormatException ex) {
+                    //throw new RuntimeException(ex);
+                    JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (HeadlessException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
+        cbCurrencyType.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                switch(cbCurrencyType.getSelectedIndex())
+                {
+                    case 0:
+                        inputLabel.setText("Input US $");
+                        break;
+                    case 1:
+                        inputLabel.setText("Input CAN $");
+                        break;
+                    case 2:
+                        inputLabel.setText("Input Euro \u20AC");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Invalid Selection", "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+
+                }
+            }
+        });
     }
 
     public JPanel getMainForm() {
